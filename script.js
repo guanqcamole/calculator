e3930742d0483f78bdb5c0c993d40fa62214e371
Quang Pham <quangpham4071@gmail.com>
2023-06-16 18:49:22 -0500

    completed UI, working on logic

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const onClicks = () => {
-    const onBtn = document.querySelector('on');
-    const offBtn = document.querySelector('off');
-    const add = document.querySelector('add');
-    const subtract = document.querySelector('subtract');
-    const multiply = document.querySelector('multiply');
-    const divide = document.querySelector('divide');
-    const equal = document.querySelector('equal');
+// const onClicks = () => {
+//     const onBtn = document.querySelector('on');
+//     const offBtn = document.querySelector('off');
+//     const add = document.querySelector('add');
+//     const subtract = document.querySelector('subtract');
+//     const multiply = document.querySelector('multiply');
+//     const divide = document.querySelector('divide');
+//     const equal = document.querySelector('equal');
 
 
+// }
+
+
+
+window.onload=function(){
+    const calculator = () => {
+
+        const onBtn = document.querySelector('on');
+        const offBtn = document.querySelector('off');
+    
+        onBtn.addEventListener('click', powerOn);
+        offBtn.addEventListener('click', powerOff);
+    
+        const powerOn = () => {
+            print('HELLO WORLD');
+        }
+    
+        const powerOff = () => {
+    
+        }
+    
+    
+        const print = (message) => {
+            const display = document.querySelector('#display');
+            display.innerHTML = message;
+        }
+    }
+    
+    calculator();
 }
-
-// const add = (a, b) => {
-//     if (typeof a != 'number' || typeof b != 'number') {
-//         return 'ERROR';
-//     }
-//     return a + b;
-// }
-
-// const subtract = (a, b) => {
-//     if (typeof a != 'number' || typeof b != 'number') {
-//         return 'ERROR';
-//     }
-//     return a - b;
-// }
-
-// const multiply = (a, b) => {
-//     if (typeof a != 'number' || typeof b != 'number') {
-//         return 'ERROR';
-//     }
-//     return a * b;
-// }
-
-// const divide = (a, b) => {
-//     if (typeof a != 'number' || typeof b != 'number') {
-//         return 'ERROR';
-//     }
-//     return a * b;
-// }
-
-// const equal 
 
